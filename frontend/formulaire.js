ed96d3c3b1695d150c75490e8826c43dbe9cddeb
firasboudhraa <fboudhraa2@gmail.com>
2024-03-27 15:12:42 +0100

    adding more verification

--- frontend/formulaire.js
+++ frontend/formulaire.js
@@ -13,9 +13,15 @@ document.getElementById('signupForm').addEventListener('submit', async (event) =
             },
             body: JSON.stringify(requestData)
         });
-        const data = await response.json();
-        console.log(data);
-        document.getElementById('signupMessage').classList.remove('hidden');
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+            document.getElementById('signupMessage').classList.remove('hidden');
+        }else{
+            console.error('Error:', response.statusText);
+            document.getElementById('error-message-inscription').classList.remove('hidden');
+        }
+
     } catch (error) {
         console.error('Error:', error);
     }
@@ -43,7 +49,7 @@ document.getElementById('signinForm').addEventListener('submit', async (event) =
             document.getElementById('signinMessage').classList.remove('hidden');
         } else {
             console.error('Error:', response.statusText);
-            document.getElementById('error-message').classList.remove('hidden');
+            document.getElementById('error-message-connexion').classList.remove('hidden');
         }
     } catch (error) {
         console.error('Error:', error);
